c1fe9790c6e4f8f66a56e60fcaf9578b6d0b8434
mrvn1 <165192424+mrvn1@users.noreply.github.com>
2024-09-08 02:41:32 +0000

    fixed

--- fracturing.c
+++ fracturing.c
@@ -33,7 +33,7 @@ double calculateDistance() {
 
 
 // With help of helper function, able to re-enter value to calculate perimeter. 
-double calculate_perimeter() {
+double calculatePerimeter() {
 
     double diameter = calculateDistance();          // Taking the distance calulated from the previous function as a base for the definition of diameter.
     double radius = diameter / 2;                   // Calculating radius by dividing  diameter by 2.
